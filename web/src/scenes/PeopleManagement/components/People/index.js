f90ad2d534f17c648d6704f7ded7924395bc34bc
Ryan <ryan@acrylic.uk>
2017-04-23 23:46:28 +0100

    UI work on people management

--- web/src/scenes/PeopleManagement/components/People/index.js
+++ web/src/scenes/PeopleManagement/components/People/index.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom';
 import Select from 'react-select';
+import ReactPaginate from 'react-paginate';
+import { BrowserRouter as Router, Route, Switch, browserHistory, IndexRedirect, IndexRoute } from 'react-router-dom';
 
 import ProfileAvatar from 'components/profileAvatar';
-import { BrowserRouter as Router, Route, Switch, browserHistory, IndexRedirect, IndexRoute } from 'react-router-dom';
+
 
 require('./style.scss');
 
@@ -11,7 +13,7 @@ require('./style.scss');
 export default class People extends Component {
 	constructor(props) {
 		super(props);
-		console.log(props);
+
 		this.state = {
 			users: [
 				{	
@@ -48,18 +50,111 @@ export default class People extends Component {
 					"jobTitle" : "Front-end Developer",
 					"Department" : "IT",
 					"Office" : "Bedford",
+				},
+				{
+					"id" : 6,
+					"name" : "Ryan Thorp",
+					"jobTitle" : "Front-end Developer",
+					"Department" : "IT",
+					"Office" : "Bedford",
+				},
+				{
+					"id" : 7,
+					"name" : "Ryan Thorp",
+					"jobTitle" : "Front-end Developer",
+					"Department" : "IT",
+					"Office" : "Bedford",
+				},
+				{
+					"id" : 8,
+					"name" : "Ryan Thorp",
+					"jobTitle" : "Front-end Developer",
+					"Department" : "IT",
+					"Office" : "Bedford",
+				},
+				{
+					"id" : 9,
+					"name" : "Ryan Thorp",
+					"jobTitle" : "Front-end Developer",
+					"Department" : "IT",
+					"Office" : "Bedford",
+				},
+				{
+					"id" : 10,
+					"name" : "Ryan Thorp",
+					"jobTitle" : "Front-end Developer",
+					"Department" : "IT",
+					"Office" : "Bedford",
+				},
+				{
+					"id" : 11,
+					"name" : "Ryan Thorp",
+					"jobTitle" : "Front-end Developer",
+					"Department" : "IT",
+					"Office" : "Bedford",
+				},
+				{
+					"id" : 12,
+					"name" : "Ryan Thorp",
+					"jobTitle" : "Front-end Developer",
+					"Department" : "IT",
+					"Office" : "Bedford",
+				},
+				{
+					"id" : 13,
+					"name" : "Ryan Thorp",
+					"jobTitle" : "Front-end Developer",
+					"Department" : "IT",
+					"Office" : "Bedford",
+				},
+				{
+					"id" : 14,
+					"name" : "Ryan Thorp",
+					"jobTitle" : "Front-end Developer",
+					"Department" : "IT",
+					"Office" : "Bedford",
 				}
 			],
+			offset: 0,
 			search: "",
+			pageCount: 0
 	    };
+	}
+	componentWillMount() {
+		this.setState({ pageCount: Math.ceil(this.state.users.length / 5) });
+		
+	}
+	searchChange( event ){
+		let user = event.target.value;
+
+		this.setState({search: user});
+
+		this.updateUsers(user);
+	}
+
+	updateUsers(queriedUser){
+
+		this.state.users.forEach( (user, index) => {
+			
+			if ( user.lastIndexOf(queriedUser, 0) === 0 ){
+				console.log(user);
+			}
+
+		});
 
 	}
-	searchChange( target ){
-		console.log(target);
-	}
+
+	handlePageClick = (data) => {
+	    let selected = data.selected;
+	    let offset = Math.ceil(selected * this.props.perPage);
+
+	    this.setState({offset: offset}, () => {
+	      this.loadCommentsFromServer();
+	    });
+	  };
 	logChange(val){
-		console.log(val);
 	}
+
 	render() {
 
 		const ShowOptions = [
@@ -71,54 +166,94 @@ export default class People extends Component {
 			<div className="people-panel block row">
 
 					<div className="col-lg-2">
-						<div className="filters-siderbar">
+						<div className="filters-siderbar outline-grey">
 							<form>
-								<label>Show</label>
-								<Select
-									name="form-field-name"
-									value="one"
-									options={ShowOptions}
-									onChange={this.logChange()}
-								/>
 
-								<label>Sort by</label>
-								<Select
-									name="form-field-name"
-									value="one"
-									options={ShowOptions}
-									onChange={this.logChange()}
-								/>
+								<fieldset>
+									<label>Show</label>
+									<Select
+										name="form-field-name"
+										value="one"
+										clearable={false}
+										searchable={false}
+										multi={false}
+										options={[
+											{value: 'all', label: 'All employees'},
+											{value: 'managers', label: 'Managers'},
+											{value: 'employees', label: 'Employees'},
+											{value: 'hierarchical', label: 'Hierarchical'}
+										]}
+										onChange={this.logChange()}
+									/>
+								</fieldset>
 
-								<label>Deparments</label>
-								<Select
-									name="form-field-name"
-									value="one"
-									options={ShowOptions}
-									onChange={this.logChange()}
-								/>
+								<fieldset>
+									<label>Sort by</label>
+									<Select
+										name="form-field-name"
+										value="one"
+										clearable={false}
+										searchable={false}
+										options={ShowOptions}
+										onChange={this.logChange()}
+									/>
+								</fieldset>
 
-								<label>Locations</label>
-								<Select
-									name="form-field-name"
-									value="one"
-									options={ShowOptions}
-									onChange={this.logChange()}
-								/>
+								<fieldset>
+									<label>Deparments</label>
+									<Select
+										name="form-field-name"
+										value="one"
+										clearable={false}
+										options={ShowOptions}
+										onChange={this.logChange()}
+									/>
+								</fieldset>
+
+								<fieldset>
+									<label>Locations</label>
+									<Select
+										name="form-field-name"
+										value="one"
+										clearable={false}
+										options={ShowOptions}
+										onChange={this.logChange()}
+									/>
+								</fieldset>
 
 							</form>
 						</div>
 					</div>
 
 					<div className="col-lg-10">
-						<div className="search">
+						<div className="search row">
 							 <form>
 								<label><i className="icon-search"></i></label>
-								<input type="text" value={this.state.search} onChange={this.searchChange()} />
+								<input className="outline-grey" type="text" value={this.state.search} onChange={ () => { this.searchChange(event) } } />
 							</form>
+						</div>
+						<div className="controller row">
+							<div class="col-lg-2 middle-xs">
+								{this.state.users.length} Employees
+							</div>
+
+							<div class="col-lg-10 middle-xs">
+								<ReactPaginate previousLabel={"previous"}
+			                       nextLabel={"next"}
+			                       breakLabel={<a href="">...</a>}
+			                       breakClassName={"break-me"}
+			                       pageCount={this.state.users}
+			                       marginPagesDisplayed={0}
+			                       pageRangeDisplayed={0}
+			                       onPageChange={ () => this.handlePageClick(event) }
+			                       containerClassName={"pagination"}
+			                       subContainerClassName={"pages pagination"}
+			                       activeClassName={"active"} />
+							</div>
 						</div>
 						{ this.state.users.map( users => {
 							return ( 
-							<div className="people-row row middle-lg"
+							<div className="people-row row middle-xs outline-grey"
 	 						key={users.id}
 							{...users} >
 								<ProfileAvatar 
